80c5771ea0a3aab3db329aa7311b4c574e4538a0
Kristian <02kristian.shyiak@gulllakecs.org>
2020-01-28 09:49:33 -0500

    Add scores, buttons, textviews, and displays
    It works

---
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lesson3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addThreeForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoForTeamA(View v) {
+        displayForTeamA(2);
+    }
+
+    public void addOneForTeamA(View v) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
