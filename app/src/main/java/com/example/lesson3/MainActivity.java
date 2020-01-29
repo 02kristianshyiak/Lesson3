d407da3dfd8737b773876ec3f2bd281aea7dfc97
Kristian <02kristian.shyiak@gulllakecs.org>
2020-01-29 09:29:52 -0500

    edit (addThreeForTeamA)  so it can add three

--- app/src/main/java/com/example/lesson3/MainActivity.java
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addThreeForTeamA(View v) {
-        displayForTeamA(3);
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
 
     public void addTwoForTeamA(View v) {
